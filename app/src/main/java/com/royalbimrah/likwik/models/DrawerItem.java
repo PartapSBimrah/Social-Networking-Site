af559dbf6c7d9890e9800c7344c80ef301323319
Kanwarpartap Singh <39325207+PartapSBimrah@users.noreply.github.com>
2020-07-11 19:37:52 +0530

    Completed Project

---
+++ app/src/main/java/com/royalbimrah/likwik/models/DrawerItem.java
@@ -0,0 +1,33 @@
+package com.royalbimrah.likwik.models;
+
+import android.view.ViewGroup;
+
+import com.royalbimrah.likwik.adapters.DrawerAdapter;
+
+/**
+ * Created by amsavarthan on 29/3/18.
+ */
+
+
+public abstract class DrawerItem<T extends DrawerAdapter.ViewHolder> {
+
+    protected boolean isChecked;
+
+    public abstract T createViewHolder(ViewGroup parent);
+
+    public abstract void bindViewHolder(T holder);
+
+    public boolean isChecked() {
+        return isChecked;
+    }
+
+    public DrawerItem setChecked(boolean isChecked) {
+        this.isChecked = isChecked;
+        return this;
+    }
+
+    public boolean isSelectable() {
+        return true;
+    }
+
+}
